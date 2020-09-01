019f3d6488c1ece3e56c63407395b270eead39a7
James Bowden <jamesbowd@gmail.com>
2020-09-01 21:01:56 +0100

    added reset and ability to change number of squares

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const container = document.querySelector(".container");
 const reset = document.querySelector(".reset");
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.className = "box";
-  container.appendChild(div);
-}
+const createGrid = (numberOfSquaresPerRow) => {
+  for (let i = 0; i < numberOfSquaresPerRow * numberOfSquaresPerRow; i++) {
+    const div = document.createElement("div");
+    div.className = "box";
+    const boxWidth = 640 / numberOfSquaresPerRow;
+    div.style.width = boxWidth + "px";
+    div.style.height = boxWidth + "px";
+    container.appendChild(div);
+  }
+};
 
-const boxes = document.querySelectorAll(".box");
+const startDrawing = () => {
+  const boxes = document.querySelectorAll(".box");
 
-for (const box of boxes) {
-  box.addEventListener("mouseover", () => {
-    box.setAttribute("style", "background-color: #c4946a");
-  });
-}
+  for (const box of boxes) {
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "#c4946a";
+      console.log("mouseover");
+    });
+  }
+};
+
+createGrid(16);
+startDrawing();
+
+reset.addEventListener("click", () => {
+  container.innerHTML = "";
+  const numberOfSquaresPerRow = prompt("Number of squares?");
+  createGrid(numberOfSquaresPerRow);
+  startDrawing();
+});
